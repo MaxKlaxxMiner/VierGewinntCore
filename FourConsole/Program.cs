62dcf79e9b6be568f0273fadce2ecda747541324
MaxKlaxxMiner <max@tetrisk.de>
2015-09-09 20:36:48 +0200

    Konsolen-Projekt hinzugefügt

---
+++ FourConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿#region # using *.*
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace FourConsole
+{
+  static class Program
+  {
+    static void Main()
+    {
+
+    }
+  }
+}
