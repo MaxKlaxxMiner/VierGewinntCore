0c3afd853ae7d9dbec148f7c07e9c8810e6291e4
MaxKlaxxMiner <max@tetrisk.de>
2015-09-09 20:33:40 +0200

    Basis-Projekt eingerichtet

---
+++ VierGewinntCore/SpielFeld.cs
@@ -0,0 +1,18 @@
+﻿#region # using *.*
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#endregion
+
+namespace VierGewinntCore
+{
+  /// <summary>
+  /// merkt sich ein 4-Gewinnt Spielfeld
+  /// </summary>
+  public class SpielFeld
+  {
+  }
+}
