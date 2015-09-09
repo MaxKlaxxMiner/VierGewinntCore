b2321c21d589bb544420ef396fc6a310092ef0ef
MaxKlaxxMiner <max@tetrisk.de>
2015-09-09 20:39:42 +0200

    UnitTest-Projekt hinzugefügt

---
+++ VierGewinntCore.Test/BasisTests.cs
@@ -0,0 +1,19 @@
+﻿#region # using *.*
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace VierGewinntCore.Test
+{
+  [TestClass]
+  public sealed class BasisTests
+  {
+    [TestMethod]
+    public void SpielfeldBasis()
+    {
+
+    }
+  }
+}
