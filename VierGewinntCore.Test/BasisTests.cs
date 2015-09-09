b0dcbc9d2eb3f9e1604909d865f1a18dccaa83fd
MaxKlaxxMiner <max@tetrisk.de>
2015-09-09 22:04:33 +0200

    SpielFeld-Basis hinzugefügt

--- VierGewinntCore.Test/BasisTests.cs
+++ VierGewinntCore.Test/BasisTests.cs
@@ -2,18 +2,310 @@
 
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable UnusedVariable
 
 #endregion
 
 namespace VierGewinntCore.Test
 {
   [TestClass]
-  public sealed class BasisTests
+  public sealed class SpielfeldValidateTests
   {
-    [TestMethod]
-    public void SpielfeldBasis()
+    #region # // --- Helper-Methoden ---
+    /// <summary>
+    /// gibt das Spielfeld als Zeichenkette zurück (gleicher Aufbau wie die Test-Felder)
+    /// </summary>
+    /// <param name="feld">Spielfeld, was zurück gegeben werden soll</param>
+    /// <returns>gefilterte Zeichenkette des Spielfeldes</returns>
+    static string GetStr(SpielFeld feld)
     {
-
+      return feld.ToString().Replace("\r", "").Replace("\n", "").ToLower();
     }
+    #endregion
+
+    #region # // --- Test1 - Leer ---
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test1F_Leer()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......" +
+        "......." +
+        "......." +
+        ".......";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    public void Test1R_Leer()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        ".......";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+    #endregion
+
+    #region # // --- Test2 - Spieler-Anzahl ---
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test2Fa_SpielerAnzahl()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        "...o...";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test2Fb_SpielerAnzahl()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......." +
+        "....x.." +
+        "....o.." +
+        ".x..x..";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test2Fc_SpielerAnzahl()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        "....o.." +
+        ".o..x..";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test2Fd_SpielerAnzahl()
+    {
+      const string Feld =
+        "xxoxoxo" +
+        "oxoxoxo" +
+        "oxoxoxo" +
+        "xoxoxox" +
+        "xoxoxox" +
+        "xoxoxox";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    public void Test2Ra_SpielerAnzahl()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        "...x...";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+
+    [TestMethod]
+    public void Test2Rb_SpielerAnzahl()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "....o.." +
+        "....x.." +
+        ".o..o.." +
+        ".x..x..";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+
+    [TestMethod]
+    public void Test2Rc_SpielerAnzahl()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......." +
+        "....x.." +
+        ".x..o.." +
+        ".o..x..";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+
+    [TestMethod]
+    public void Test2Rd_SpielerAnzahl()
+    {
+      const string Feld =
+        "oxoxoxo" +
+        "oxoxoxo" +
+        "oxoxoxo" +
+        "xoxoxox" +
+        "xoxoxox" +
+        "xoxoxox";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+    #endregion
+
+    #region # // --- Test3 - Feld-Lücken ---
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test3Fa_FeldLücken()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        ".x....." +
+        ".......";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test3Fb_FeldLücken()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        ".o....." +
+        "......." +
+        ".x....." +
+        ".x.....";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test3Fc_FeldLücken()
+    {
+      const string Feld =
+        "......o" +
+        "......." +
+        "......." +
+        "......." +
+        ".xox..." +
+        ".xxo...";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void Test3Fd_FeldLücken()
+    {
+      const string Feld =
+        "oxoxoxo" +
+        "oxoxoxo" +
+        "oxox.xo" +
+        "xoxoxox" +
+        "xoxoxox" +
+        "xoxoxox";
+
+      var feld = new SpielFeld(Feld);
+    }
+
+    [TestMethod]
+    public void Test3Ra_FeldLücken()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        "......." +
+        ".x.....";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+
+    [TestMethod]
+    public void Test3Rb_FeldLücken()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        ".o....." +
+        ".o....." +
+        ".x....." +
+        ".x.....";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+
+    [TestMethod]
+    public void Test3Rc_FeldLücken()
+    {
+      const string Feld =
+        "......." +
+        "......." +
+        "......x" +
+        "...o..x" +
+        ".xox..x" +
+        ".xxoooo";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+
+    [TestMethod]
+    public void Test3Rd_FeldLücken()
+    {
+      const string Feld =
+        "oxox.xo" +
+        "oxoxoxo" +
+        "oxoxoxo" +
+        "xoxoxox" +
+        "xoxoxox" +
+        "xoxoxox";
+
+      var feld = new SpielFeld(Feld);
+
+      Assert.AreEqual(Feld, GetStr(feld));
+    }
+    #endregion
   }
 }
--- VierGewinntCore/SpielFeld.cs
+++ VierGewinntCore/SpielFeld.cs
@@ -1,9 +1,9 @@
 ﻿#region # using *.*
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+// ReSharper disable MemberCanBePrivate.Global
 
 #endregion
 
@@ -12,7 +12,140 @@ namespace VierGewinntCore
   /// <summary>
   /// merkt sich ein 4-Gewinnt Spielfeld
   /// </summary>
-  public class SpielFeld
+  public sealed class SpielFeld
   {
+    #region # // --- Konstanten ---
+    /// <summary>
+    /// gibt die Breite des Spielfeldes an (Default: 7)
+    /// </summary>
+    public const int FeldBreite = 7;
+
+    /// <summary>
+    /// gibt die Höhe des Spielfeldes an (Default: 6)
+    /// </summary>
+    public const int FeldHöhe = 6;
+
+    /// <summary>
+    /// gibt die Anzahl der Spielfelder an (7 * 6 = 42)
+    /// </summary>
+    public const int FeldAnzahl = FeldBreite * FeldHöhe;
+    #endregion
+
+    #region # // --- Variablen ---
+    /// <summary>
+    /// das eigentliche Spielfeld
+    /// </summary>
+    readonly byte[] feld;
+    #endregion
+
+    #region # // --- Methoden ---
+    /// <summary>
+    /// überprüft das Spielfeld auf Gültigkeit
+    /// </summary>
+    /// <returns>true, wenn das Spielfeld gültig ist sonst false</returns>
+    bool Validate()
+    {
+      // --- Felder-Anzahl und Inhalt prüfen ---
+
+      if (feld.Length != FeldAnzahl) return false;
+      if (feld.Any(b => b > 2)) return false;
+
+
+      // --- Spieler Anzahl prüfen ---
+
+      int spielerX = feld.Count(b => b == 1);
+      int spielerO = feld.Count(b => b == 2);
+
+      if (spielerX != spielerO && spielerX - 1 != spielerO) return false;
+
+
+      // --- auf ungültige Lücken prüfen ---
+
+      for (int x = 0; x < FeldBreite; x++)
+      {
+        bool find = false;
+        for (int y = 0; y < FeldHöhe; y++)
+        {
+          if (feld[x + y * FeldBreite] != 0)
+          {
+            find = true;
+          }
+          else
+          {
+            if (find) return false;
+          }
+        }
+      }
+
+
+      // --- alles soweit ok ---
+
+      return true;
+    }
+
+    /// <summary>
+    /// gibt das Spielfeld als Zeichenkette zurück
+    /// </summary>
+    /// <returns>sichbares Spielfeld als Zeichenkette</returns>
+    public override string ToString()
+    {
+      var ausgabe = new StringBuilder(FeldAnzahl + FeldBreite * 2);
+
+      for (int y = 0; y < FeldHöhe; y++)
+      {
+        for (int x = 0; x < FeldBreite; x++)
+        {
+          switch (feld[x + y * FeldBreite])
+          {
+            case 1: ausgabe.Append('X'); break;
+            case 2: ausgabe.Append('O'); break;
+            default: ausgabe.Append('.'); break;
+          }
+        }
+        ausgabe.AppendLine();
+      }
+
+      return ausgabe.ToString();
+    }
+    #endregion
+
+    #region # // --- Konstruktor ---
+    /// <summary>
+    /// Konstruktor
+    /// </summary>
+    /// <param name="spielfeldString">SpielfeldDaten</param>
+    public SpielFeld(string spielfeldString)
+    {
+      feld = spielfeldString.ToCharArray().Select(c =>
+      {
+        switch (c)
+        {
+          case ' ':
+          case '-':
+          case '.':
+          case '\r':
+          case '\n':
+          case '\t':
+          return (byte)0;
+
+          case 'x':
+          case 'X':
+          case '1':
+          case '+':
+          return (byte)1;
+
+          case 'o':
+          case 'O':
+          case '0':
+          case '2':
+          return (byte)2;
+
+          default: return (byte)255;
+        }
+      }).Where(f => f < 255).ToArray();
+
+      if (!Validate()) throw new Exception("ungültiges Spielfeld!");
+    }
+    #endregion
   }
 }
