8fa5f30e3a3a70319af4b22f2de7a2cdc5a684d9
MaxKlaxxMiner <max@tetrisk.de>
2015-09-11 21:38:31 +0200

    neue (schnellere) Methode für "CheckGewinn"

--- VierGewinntCore/SpielFeld.cs
+++ VierGewinntCore/SpielFeld.cs
@@ -50,6 +50,161 @@ namespace VierGewinntCore
 
     #region # // --- Methoden ---
 
+    #region # // --- bool CheckGewinn() - prüft, ob ein bestimmter Spielzug zum Gewinn geführt hat ---
+    /// <summary>
+    /// merkt sich die statische Map, welche alle direkten Prüfpositionen des Spielfeldes enthält
+    /// </summary>
+    static readonly byte[] GewinnMap = GeneriereGewinnMap();
+
+    /// <summary>
+    /// generiert die "GewinnMap"
+    /// </summary>
+    /// <returns>fertig erstellte Gewinn-Map</returns>
+    static byte[] GeneriereGewinnMap()
+    {
+      var map = new byte[FeldAnzahl * 64];
+
+      for (int py = 0; py < FeldHöhe; py++)
+      {
+        for (int px = 0; px < FeldBreite; px++)
+        {
+          int pOfs = (px + py * FeldBreite) * 64;
+          int p = pOfs + 1;
+
+          // --- diagonal lo -> ru ---
+
+          if (px > 2 && px < FeldBreite && py > 2 && py < FeldHöhe)
+          {
+            map[p++] = (byte)(px - 3 + (py - 3) * FeldBreite);
+            map[p++] = (byte)(px - 2 + (py - 2) * FeldBreite);
+            map[p++] = (byte)(px - 1 + (py - 1) * FeldBreite);
+          }
+
+          if (px > 1 && px < FeldBreite - 1 && py > 1 && py < FeldHöhe - 1)
+          {
+            map[p++] = (byte)(px - 2 + (py - 2) * FeldBreite);
+            map[p++] = (byte)(px - 1 + (py - 1) * FeldBreite);
+            map[p++] = (byte)(px + 1 + (py + 1) * FeldBreite);
+          }
+
+          if (px > 0 && px < FeldBreite - 2 && py > 0 && py < FeldHöhe - 2)
+          {
+            map[p++] = (byte)(px - 1 + (py - 1) * FeldBreite);
+            map[p++] = (byte)(px + 1 + (py + 1) * FeldBreite);
+            map[p++] = (byte)(px + 2 + (py + 2) * FeldBreite);
+          }
+
+          if (px > -1 && px < FeldBreite - 3 && py > -1 && py < FeldHöhe - 3)
+          {
+            map[p++] = (byte)(px + 1 + (py + 1) * FeldBreite);
+            map[p++] = (byte)(px + 2 + (py + 2) * FeldBreite);
+            map[p++] = (byte)(px + 3 + (py + 3) * FeldBreite);
+          }
+
+
+          // --- diagonal lu -> ro
+
+          if (px > 2 && px < FeldBreite && py > -1 && py < FeldHöhe - 3)
+          {
+            map[p++] = (byte)(px - 3 + (py + 3) * FeldBreite);
+            map[p++] = (byte)(px - 2 + (py + 2) * FeldBreite);
+            map[p++] = (byte)(px - 1 + (py + 1) * FeldBreite);
+          }
+
+          if (px > 1 && px < FeldBreite - 1 && py > 0 && py < FeldHöhe - 2)
+          {
+            map[p++] = (byte)(px - 2 + (py + 2) * FeldBreite);
+            map[p++] = (byte)(px - 1 + (py + 1) * FeldBreite);
+            map[p++] = (byte)(px + 1 + (py - 1) * FeldBreite);
+          }
+
+          if (px > 0 && px < FeldBreite - 2 && py > 1 && py < FeldHöhe - 1)
+          {
+            map[p++] = (byte)(px - 1 + (py + 1) * FeldBreite);
+            map[p++] = (byte)(px + 1 + (py - 1) * FeldBreite);
+            map[p++] = (byte)(px + 2 + (py - 2) * FeldBreite);
+          }
+
+          if (px > -1 && px < FeldBreite - 3 && py > 2 && py < FeldHöhe)
+          {
+            map[p++] = (byte)(px + 1 + (py - 1) * FeldBreite);
+            map[p++] = (byte)(px + 2 + (py - 2) * FeldBreite);
+            map[p++] = (byte)(px + 3 + (py - 3) * FeldBreite);
+          }
+
+
+          // --- waagerecht ---
+
+          if (px > 2 && px < FeldBreite)
+          {
+            map[p++] = (byte)(px - 3 + py * FeldBreite);
+            map[p++] = (byte)(px - 2 + py * FeldBreite);
+            map[p++] = (byte)(px - 1 + py * FeldBreite);
+          }
+
+          if (px > 1 && px < FeldBreite - 1)
+          {
+            map[p++] = (byte)(px - 2 + py * FeldBreite);
+            map[p++] = (byte)(px - 1 + py * FeldBreite);
+            map[p++] = (byte)(px + 1 + py * FeldBreite);
+          }
+
+          if (px > 0 && px < FeldBreite - 2)
+          {
+            map[p++] = (byte)(px - 1 + py * FeldBreite);
+            map[p++] = (byte)(px + 1 + py * FeldBreite);
+            map[p++] = (byte)(px + 2 + py * FeldBreite);
+          }
+
+          if (px > -1 && px < FeldBreite - 3)
+          {
+            map[p++] = (byte)(px + 1 + py * FeldBreite);
+            map[p++] = (byte)(px + 2 + py * FeldBreite);
+            map[p++] = (byte)(px + 3 + py * FeldBreite);
+          }
+
+
+          // --- senkrecht ---
+
+          if (py < FeldHöhe - 3)
+          {
+            map[p++] = (byte)(px + (py + 1) * FeldBreite);
+            map[p++] = (byte)(px + (py + 2) * FeldBreite);
+            map[p++] = (byte)(px + (py + 3) * FeldBreite);
+          }
+
+          map[pOfs] = (byte)(p - pOfs); // Länge abspeichern
+        }
+      }
+
+      return map;
+    }
+
+    /// <summary>
+    /// prüft, ob ein bestimmter Spielzug zum Gewinn geführt hat
+    /// </summary>
+    /// <param name="pos">Position des letzten gesetzten Steines, welche geprüft werden soll</param>
+    /// <returns>true, wenn gewonnen wurde</returns>
+    internal bool CheckGewinn(int pos)
+    {
+      int mapPos = pos * 64;
+      int len = mapPos + GewinnMap[mapPos];
+      int f = feld[pos];
+
+      if (f == 0) return false;
+
+      for (int p = mapPos + 1; p < len; p += 3)
+      {
+        int i1 = GewinnMap[p];
+        int i2 = GewinnMap[p + 1];
+        int i3 = GewinnMap[p + 2];
+
+        if (feld[i1] == f && feld[i2] == f && feld[i3] == f) return true;
+      }
+
+      return false;
+    }
+
     /// <summary>
     /// prüft, ob eine Kette zusammenhängend gewinnt
     /// </summary>
@@ -61,7 +216,7 @@ namespace VierGewinntCore
     /// <param name="x3">X-Pos vom dritten prüfenden Feld</param>
     /// <param name="y3">Y-Pos vom dritten prüfenden Feld</param>
     /// <returns>true, wenn die Spielerfarbe mit allen Feldern übereinstimmt</returns>
-    bool CheckGewinnKette(int f, int x1, int y1, int x2, int y2, int x3, int y3)
+    bool CheckGewinnKette_Old(int f, int x1, int y1, int x2, int y2, int x3, int y3)
     {
       if (x1 < 0 || x2 < 0 || x3 < 0 || y1 < 0 || y2 < 0 || y3 < 0) return false;
       if (x1 >= FeldBreite || x2 >= FeldBreite || x3 >= FeldBreite || y1 >= FeldHöhe || y2 >= FeldHöhe || y3 >= FeldHöhe) return false;
@@ -76,35 +231,36 @@ namespace VierGewinntCore
     /// </summary>
     /// <param name="pos">Position des letzten gesetzten Steines, welche geprüft werden soll</param>
     /// <returns>true, wenn gewonnen wurde</returns>
-    internal bool CheckGewinn(int pos)
+    // ReSharper disable once UnusedMember.Global
+    internal bool CheckGewinn_Old(int pos)
     {
       int f = feld[pos];
+      if (f == 0) return false;
 
       int x = pos % FeldBreite;
       int y = pos / FeldBreite;
 
-      return CheckGewinnKette(f, x - 3, y, x - 2, y, x - 1, y) ||
-             CheckGewinnKette(f, x - 2, y, x - 1, y, x + 1, y) ||
-             CheckGewinnKette(f, x - 1, y, x + 1, y, x + 2, y) ||
-             CheckGewinnKette(f, x + 1, y, x + 2, y, x + 3, y) ||
+      return CheckGewinnKette_Old(f, x - 3, y, x - 2, y, x - 1, y) ||
+             CheckGewinnKette_Old(f, x - 2, y, x - 1, y, x + 1, y) ||
+             CheckGewinnKette_Old(f, x - 1, y, x + 1, y, x + 2, y) ||
+             CheckGewinnKette_Old(f, x + 1, y, x + 2, y, x + 3, y) ||
 
-             CheckGewinnKette(f, x, y - 3, x, y - 2, x, y - 1) ||
-             CheckGewinnKette(f, x, y - 2, x, y - 1, x, y + 1) ||
-             CheckGewinnKette(f, x, y - 1, x, y + 1, x, y + 2) ||
-             CheckGewinnKette(f, x, y + 1, x, y + 2, x, y + 3) ||
+             CheckGewinnKette_Old(f, x, y - 3, x, y - 2, x, y - 1) ||
+             CheckGewinnKette_Old(f, x, y - 2, x, y - 1, x, y + 1) ||
+             CheckGewinnKette_Old(f, x, y - 1, x, y + 1, x, y + 2) ||
+             CheckGewinnKette_Old(f, x, y + 1, x, y + 2, x, y + 3) ||
 
-             CheckGewinnKette(f, x - 3, y - 3, x - 2, y - 2, x - 1, y - 1) ||
-             CheckGewinnKette(f, x - 2, y - 2, x - 1, y - 1, x + 1, y + 1) ||
-             CheckGewinnKette(f, x - 1, y - 1, x + 1, y + 1, x + 2, y + 2) ||
-             CheckGewinnKette(f, x + 1, y + 1, x + 2, y + 2, x + 3, y + 3) ||
+             CheckGewinnKette_Old(f, x - 3, y - 3, x - 2, y - 2, x - 1, y - 1) ||
+             CheckGewinnKette_Old(f, x - 2, y - 2, x - 1, y - 1, x + 1, y + 1) ||
+             CheckGewinnKette_Old(f, x - 1, y - 1, x + 1, y + 1, x + 2, y + 2) ||
+             CheckGewinnKette_Old(f, x + 1, y + 1, x + 2, y + 2, x + 3, y + 3) ||
 
-             CheckGewinnKette(f, x + 3, y - 3, x + 2, y - 2, x + 1, y - 1) ||
-             CheckGewinnKette(f, x + 2, y - 2, x + 1, y - 1, x - 1, y + 1) ||
-             CheckGewinnKette(f, x + 1, y - 1, x - 1, y + 1, x - 2, y + 2) ||
-             CheckGewinnKette(f, x - 1, y + 1, x - 2, y + 2, x - 3, y + 3) ||
-        false
-      ;
+             CheckGewinnKette_Old(f, x + 3, y - 3, x + 2, y - 2, x + 1, y - 1) ||
+             CheckGewinnKette_Old(f, x + 2, y - 2, x + 1, y - 1, x - 1, y + 1) ||
+             CheckGewinnKette_Old(f, x + 1, y - 1, x - 1, y + 1, x - 2, y + 2) ||
+             CheckGewinnKette_Old(f, x - 1, y + 1, x - 2, y + 2, x - 3, y + 3);
     }
+    #endregion
 
     /// <summary>
     /// überprüft das Spielfeld auf Gültigkeit und setzt die Felder "Status" und "ZugNummer"
