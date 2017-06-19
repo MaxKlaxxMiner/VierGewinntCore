2ea55c881be31098b50471460172e45faa4d9f05
MaxKlaxxMiner <max@tetrisk.de>
2017-06-19 20:23:08 +0200

    basis-counter hinzugefügt

--- Research/Program.cs
+++ Research/Program.cs
@@ -8,9 +8,45 @@ namespace Research
 {
   class Program
   {
+    static readonly char[] FieldChars = { ' ', 'x', 'o' };
+
+    static IEnumerable<string> RowCounterBase(int len)
+    {
+      var cc = new byte[len];
+      var chars = new char[len];
+      for (; ; )
+      {
+        for (int i = 0; i < chars.Length; i++) chars[i] = FieldChars[cc[i]];
+        yield return new string(chars);
+        int p = cc.Length - 1;
+        while (++cc[p] == FieldChars.Length)
+        {
+          cc[p] = 1;
+          p--;
+          if (p < 0) yield break;
+        }
+      }
+    }
+
+    /// <summary>
+    /// Prüfmethode zum zählen, wieviel Varianten pro Spalte möglich sind
+    /// </summary>
+    static void RowCounter()
+    {
+      int count = 0;
+      foreach (var row in RowCounterBase(6))
+      {
+        Console.WriteLine("{0,3} : \"{1}\"", count, row);
+        count++;
+      }
+      Console.WriteLine();
+      Console.WriteLine("Total-Count: " + count);
+    }
+
     static void Main(string[] args)
     {
-
+      RowCounter();
+      Console.ReadLine();
     }
   }
 }
