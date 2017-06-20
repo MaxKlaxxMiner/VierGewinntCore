583b51fc71ec96e7036e2315433d3de5335b8741
MaxKlaxxMiner <max@tetrisk.de>
2017-06-20 23:41:43 +0200

    step

--- Research/Program.cs
+++ Research/Program.cs
@@ -16,16 +16,33 @@ namespace Research
     /// </summary>
     static readonly char[] FieldChars = { ' ', 'x', 'o' };
     /// <summary>
+    /// Anzahl der Felder, welche übeinander in einer Reihe liegen können
+    /// </summary>
+    const int RowHeight = 6;
+    /// <summary>
     /// 127 Varianten, die in einer Senkrechten Reihe vorkommen können (6 Felder hoch)
     /// </summary>
-    static readonly string[] Rows = RowCounterBase(6).ToArray();
+    static readonly string[] Rows = RowCounterBase(RowHeight).ToArray();
+
+    /// <summary>
+    /// prüft, ob eine Spalte im aktiven Spielfeld gültig ist 
+    /// </summary>
+    /// <param name="row"></param>
+    /// <returns></returns>
+    static bool ValidRow(string row)
+    {
+      return !row.Contains("xxxx") && !row.Contains("oooo");
+    }
 
     /// <summary>
     /// Methode zum analysieren und prüfen, ob bestimmte Felder direkt mit Bit-Mustern abgefragt werden können
     /// </summary>
     static void BitChecker()
     {
+      var xMap = Enumerable.Range(0, RowHeight).Select(i => Rows.Where(r => r[RowHeight - 1 - i] == 'x').ToArray()).ToArray();
+      var oMap = Enumerable.Range(0, RowHeight).Select(i => Rows.Where(r => r[RowHeight - 1 - i] == 'o').ToArray()).ToArray();
 
+      int stop = 0;
     }
 
     static void Main(string[] args)
