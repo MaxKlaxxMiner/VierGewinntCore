fe9502bebeec4a39a4fc26cd0528c2d3e27130eb
MaxKlaxxMiner <max@tetrisk.de>
2017-06-20 21:00:29 +0200

    bitchecker vorbereitet

--- Research/Program.cs
+++ Research/Program.cs
@@ -8,11 +8,32 @@ namespace Research
 {
   static partial class Program
   {
+    /// <summary>
+    /// mögliche Spielfelder
+    /// ' ' = frei
+    /// 'x' = Spieler 1
+    /// 'o' = Spieler 2
+    /// </summary>
     static readonly char[] FieldChars = { ' ', 'x', 'o' };
+    /// <summary>
+    /// 127 Varianten, die in einer Senkrechten Reihe vorkommen können (6 Felder hoch)
+    /// </summary>
+    static readonly string[] Rows = RowCounterBase(6).ToArray();
+
+    /// <summary>
+    /// Methode zum analysieren und prüfen, ob bestimmte Felder direkt mit Bit-Mustern abgefragt werden können
+    /// </summary>
+    static void BitChecker()
+    {
+
+    }
 
     static void Main(string[] args)
     {
-      RowCounter();
+      // RowCounter();
+
+      BitChecker();
+
       Console.ReadLine();
     }
   }
